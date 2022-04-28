be5c30d965680b7a52972efa35036f84d529f554
DiffuseHyperion <xieyingjie08@gmail.com>
2022-04-28 20:07:05 +0800

    echo stuff

--- installVagrant.sh
+++ installVagrant.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # most code copied from https://medium.com/axon-technologies/installing-a-windows-virtual-machine-in-a-linux-docker-container-c78e4c3f9ba1
 if [ "$EUID" -ne 0 ]
-  then echo Please run as root!
+  then echo "Please run as root!"
   exit
 fi
 
-echo installVagrant.sh successfully run!
-echo Where should vagrant be located?
+echo "installVagrant.sh successfully run!"
+echo "Where should vagrant be located? Ideally, it should be in /home/(username)/vagrant, to avoid permission issues. If you decide not too, you will need to chown the files yourself."
 read dir
 echo Storing files at $dir.
 
@@ -25,8 +25,8 @@ vagrant_latest_version=$(curl -s https://checkpoint-api.hashicorp.com/v1/check/v
 echo $vagrant_latest_version
 curl -O https://releases.hashicorp.com/vagrant/$(echo $vagrant_latest_version)/vagrant_$(echo $vagrant_latest_version)_x86_64.deb
 dpkg -i vagrant_$(echo $vagrant_latest_version)_x86_64.deb
-
-chown diffusehyperion:diffusehyperion -R $dir/.vagrant.d
+mv ~/.vagrant.d $dir/vagrant
+vagrant plugin install vagrant-libvirt
 
 mkdir win10
 cd win10
@@ -38,9 +38,13 @@ cd libvirt
 mkdir images
 virsh pool-define-as --name default --type dir --target $dir/libvirt/images/
 
-echo export VAGRANT_HOME=$dir/.vagrant.d >> /etc/environment
-echo export VAGRANT_DOTFILE_PATH=$dir/.vagrant.d >> /etc/environment
+echo export VAGRANT_HOME=$dir/vagrant >> ~/.bash_profile
+echo export VAGRANT_DOTFILE_PATH=vagrant >> ~/.bash_profile
+source ~/.bash_profile
 
-echo Install complete! You may need to run the following command:
-echo vagrant plugin install vagrant-libvirt
+echo "Install complete! You may need to run the following command:"
+echo "vagrant plugin install vagrant-libvirt"
+echo Afterwards, cd into $dir/win10 and do:
+echo "VAGRANT_DEFAULT_PROVIDER=libvirt vagrant up"
+echo "The disk size might be set to 50 GB, even if Vagrantfile says otherwise. Windows 10's partition is very likely to not occupy the disk fully. Extend Window's partition to fix this.
 
